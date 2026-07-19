171a990a4d1963b411f0a22eea56951be5efde68
2201115015dj@cyber-u.ac.jp <2201115015dj@cyber-u.ac.jp>
2026-07-20 05:10:07 +0900

    add env

--- routes/cat.js
+++ routes/cat.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+
+const  cors=require('cors')
+router.use(cors());
+
 router.get('/', async (req, res) => {
-request('https://api.thecatapi.com/v1/images/search', function (error, response,
+request('http://localhost:30016/cat', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const cors = require('cors');
+require('dotenv').config();
+
 const {MongoClient}=require("mongodb");
-const uri="";
+const uri=process.env.MONGODB_URI;
+//const uri="mongodb+srv://2201115015dj_db_user:NWzZZPDwCXihU54W@db1.7jytvvf.mongodb.net/?appName=db";
+
 const client=new MongoClient(uri);
 
 router.get('/',async(req,res)=>{
